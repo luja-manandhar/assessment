a2f7f4e6b756d23ddf62cf64a55e877386384621
Luja Manandhar <manandharluja@gmail.com>
2024-05-28 13:25:30 +0545

    navbar and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent) },
   { path: 'products', loadComponent: () => import('./components/product-list/product-list.component').then(c => c.ProductListComponent) },
   { path: 'products/:id', loadComponent: () => import('./components/product-detail/product-detail.component').then(c => c.ProductDetailComponent) },
   { path: 'products/:id/edit', loadComponent: () => import('./components/product-edit/product-edit.component').then(c => c.ProductEditComponent) },
